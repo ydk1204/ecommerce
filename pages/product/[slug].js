d0d318fc23ce1e0787c7bfa77273d400184d54db
ydk1204 <ejrbdi@naver.com>
2022-08-29 21:44:42 +0900

    결제 화면

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,31 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
-import { client, urlFor } from '../../lib/client'
-import { Product } from "../../components"
+import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
 import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  }
 
   return (
     <div>
-      <div className='product-detail-container'>
+      <div className="product-detail-container">
         <div>
-          <div className='image-container'>
-            <img className='product-detail-image' src={urlFor(image && image[index])} alt="detail-image" />
+          <div className="image-container">
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
-          <div className='small-images-container'>
+          <div className="small-images-container">
             {image?.map((item, i) => (
-              <img src={urlFor(item)} className={i === index ? 'small-image selected-image' : 'small-image'} onMouseEnter={() => setIndex(i)} />
+              <img 
+                key={i}
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
             ))}
           </div>
         </div>
-        <div className='product-detail-desc'>
+
+        <div className="product-detail-desc">
           <h1>{name}</h1>
-          <div className='reviews'>
+          <div className="reviews">
             <div>
               <AiFillStar />
               <AiFillStar />
@@ -39,41 +51,31 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <h4>Details: </h4>
           <p>{details}</p>
-          <p className='price'>${price}</p>
-          <div className='quantity'>
-            <h3>Quantity :</h3>
-            <p className='quantity-desc'>
-              <span className='minus' onClick={decQty}>
-                <AiOutlineMinus />
-              </span>
-              <span className='num' onClick="">
-                {qty}
-              </span>
-              <span className='plus' onClick={incQty}>
-                <AiOutlinePlus />
-              </span>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
-          <div className='buttons'>
-            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
-              Add to Cart
-            </button>
-            <button type="button" className="buy-now" onClick="">
-              Buy Now
-            </button>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
         </div>
       </div>
 
-      <div className='maylike-products-wrapper'>
-        <h2>You may also like</h2>
-        <div className='marquee'>
-          <div className='maylike-products-container track'>
-            {products.map((item) => (
-              <Product key={item._id} product={item} />
-            ))}
+      <div className="maylike-products-wrapper">
+          <h2>You may also like</h2>
+          <div className="marquee">
+            <div className="maylike-products-container track">
+              {products.map((item) => (
+                <Product key={item._id} product={item} />
+              ))}
+            </div>
           </div>
-        </div>
       </div>
     </div>
   )
@@ -90,7 +92,7 @@ export const getStaticPaths = async () => {
   const products = await client.fetch(query);
 
   const paths = products.map((product) => ({
-    params: {
+    params: { 
       slug: product.slug.current
     }
   }));
@@ -101,18 +103,18 @@ export const getStaticPaths = async () => {
   }
 }
 
-export const getStaticProps = async ({params: {slug}}) => {
+export const getStaticProps = async ({ params: { slug }}) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-  const productsQuery = '*[_type == "product"]';
-
+  const productsQuery = '*[_type == "product"]'
+  
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
   console.log(product);
 
   return {
-    props: { product, products },
-  };
-};
+    props: { products, product }
+  }
+}
 
 export default ProductDetails
